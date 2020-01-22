c6880c06955ce81fca4d5605fa7a61a2f3b81a5c
Igor Walace Cuevas <igor.w.cuevas@gmail.com>
2020-01-22 01:23:38 -0300

    Primeiros componentes

---
+++ rocketshoes/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
+import GlobalStyles from './styles/global';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GlobalStyles />
+      <Header />
+      <Routes />
+    </BrowserRouter>
+  );
+}
+
+export default App;
